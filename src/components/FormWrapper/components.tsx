9b4ac635f8353200889367fcdb1a4fd59f300963
Devian <devian@Devians-MacBook-Air.local>
2023-04-27 00:42:03 +0900

    fix Form Wrapper

--- src/components/FormWrapper/components.tsx
+++ src/components/FormWrapper/components.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import {
-  View,
   StyleSheet,
-  Platform,
   TouchableWithoutFeedback,
   KeyboardAvoidingView,
   Keyboard,
 } from 'react-native';
 import Props from './type';
-
+import Wrapper from '../Wrapper';
 const Component = ({children, type = 'default'}: Props) => {
-  const containerStyle = {
-    ...styles.container,
-    ...(type === 'gray' && styles.containerGray),
-    ...(type === 'white' && styles.containerWhite),
-  };
   return (
-    <View style={containerStyle}>
+    <Wrapper>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <KeyboardAvoidingView
           style={styles.container}
@@ -25,21 +18,13 @@ const Component = ({children, type = 'default'}: Props) => {
           {children}
         </KeyboardAvoidingView>
       </TouchableWithoutFeedback>
-    </View>
+    </Wrapper>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    paddingTop: Platform.OS === 'android' ? 25 : 0, // add extra padding on Android
-  },
-  containerGray: {
-    backgroundColor: '#333',
-  },
-  containerWhite: {
-    backgroundColor: '#fff',
   },
 });
 
--- src/components/Wrapper/type.ts
+++ src/components/Wrapper/type.ts
@@ -2,7 +2,6 @@ import {ReactNode} from 'react';
 type Props = {
   children: ReactNode;
   type?: 'default' | 'gray' | 'white';
-  align?: 'flex-start' | 'center' | 'flex-end';
 };
 
 export default Props;
